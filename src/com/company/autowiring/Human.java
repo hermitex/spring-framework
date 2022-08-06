9d3c75a51bf757113db585de9abe223e89b1d07e
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-06 12:35:22 +0300

    Done autowiring by constructor using Spring

--- src/com/company/autowiring/Human.java
+++ src/com/company/autowiring/Human.java
@@ -3,14 +3,18 @@ package com.company.autowiring;
 public class Human {
     private Heart heart;
 
+    public Human(Heart heart) {
+        this.heart = heart;
+    }
+
     public void setHeart(Heart heart) {
         this.heart = heart;
     }
 
-    public void startBeating(){
-        if(heart != null){
+    public void startBeating() {
+        if (heart != null) {
             heart.beat();
-        }else{
+        } else {
             System.out.println("You're dead!");
         }
 
