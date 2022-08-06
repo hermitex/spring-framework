54b06d2f5add6cfe9bceb5f4f0e7d92f490573b9
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-06 12:51:59 +0300

    Done autowiring by Autowiring in Java constructor using Spring

--- src/com/company/autowiring/Human.java
+++ src/com/company/autowiring/Human.java
@@ -1,8 +1,14 @@
 package com.company.autowiring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Human {
     private Heart heart;
 
+    public Human() {
+    }
+
+    @Autowired
     public Human(Heart heart) {
         this.heart = heart;
     }
